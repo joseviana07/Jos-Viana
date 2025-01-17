b258abaddeecd1fc82884edfa3cc246e1eb0cc8f
joseviana07 <jose.viana@correo.unimet.edu.ve>
2025-01-16 21:50:24 -0400

    Add files via upload

---
+++ Primera clase.py
@@ -0,0 +1,8 @@
+number = int(input("Please enter a number:"))
+number2 = int(input("Please enter a number 2:"))
+print("El resultado de la suma es:",number + number2)
+print("El resultado de la resta es:",number - number2)
+print("El resultado de la multiplicación es:",number * number2)
+print("El resultado de la división es:",number / number2)
+
+
